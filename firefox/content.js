d9fc35559d86bf4e5a689ef58406c527eeda0800
ceeprus <cihanbeykoroglu@gmail.com>
2024-09-24 13:00:31 +0300

    update

--- firefox/content.js
+++ firefox/content.js
@@ -1,19 +1,14 @@
 javascript:(function() {
-  var buttons = document.querySelectorAll('.oclb');
-  buttons.forEach(function(button) {
-      button.click();
-  });
-})();
+  document.querySelectorAll('.oclb').forEach(button => button.click());
 
-(function() {
-  const observer = new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      for (const node of mutation.addedNodes) {
-        if (node.nodeType === Node.ELEMENT_NODE && node.classList.contains('.oclb')) {
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      mutation.addedNodes.forEach(node => {
+        if (node.nodeType === Node.ELEMENT_NODE && node.classList.contains('oclb')) {
           node.click();
         }
-      }
-    }
+      });
+    });
   });
 
   observer.observe(document.body, { childList: true, subtree: true });
